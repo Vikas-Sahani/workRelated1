f5f74c937720f6d9fe87285ca27e3d592ac09f78
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-01-04 23:10:12 +0530

    almost done, just remaining to add calender & some more features

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
-// import { useRef } from 'react';
+import ghanshyamSahani from "./img/GhanshyamSahani.png"
 import { useState } from 'react';
 function App() {
   const[emp1, setEmp1] = useState(0);
   const[emp2, setEmp2] = useState(0);
   const[emp3, setEmp3] = useState(0);
   const rate = 500;
+  const[moneyTakenByEmp1, setMoneyTakenByEmp1] = useState(0);
+  const[moneyTakenByEmp2, setMoneyTakenByEmp2] = useState(0);
+  const[moneyTakenByEmp3, setMoneyTakenByEmp3] = useState(0);
+  
+  const[remAmt1, setRemAmt1] = useState(0);
+  const[remAmt2, setRemAmt2] = useState(0);
+  const[remAmt3, setRemAmt3] = useState(0);
   return (
     <div className="App">
-      <h1>Owner Name</h1>
+      <div className='owner'>
+        <img src={ghanshyamSahani} alt='ownerImg'/>
+      </div>
+      <h2>Total Attendance</h2>
       <table >
         <thead>
           <tr>
@@ -24,37 +34,64 @@ function App() {
             <tr key={cnt}>
               <td>{cnt}</td>
 
-              <td className='amt_att'><input type="checkbox" onChange={(e)=>{
-                if(e.target.checked){
-                  setEmp1(emp1+1);
-                  e.target.disabled= true;
-                }
-              }} />
+              <td className='amt_att'>
+                <input type="checkbox" onChange={(e)=>{
+                  if(e.target.checked){
+                    setEmp1(emp1+1);
+                    e.target.disabled= true;
+                    e.target.nextSibling.disabled=true;
+                    setRemAmt1(moneyTakenByEmp1+remAmt1)
+                    setMoneyTakenByEmp1(0);
+                  }
+                }} />
+                <input type="text" onChange={(e)=>{
+                  if(isNaN(e.target.value)){
+                    alert("Please Enter a Number")
+                    e.target.value="";
+                  } else{
+                    setMoneyTakenByEmp1(Number(e.target.value));
+                  }
+                }}/>
+              </td>
+
+              <td className='amt_att'>
+                <input type="checkbox" onChange={(e)=>{
+                  if(e.target.checked){
+                    setEmp2(emp2+1);
+                    e.target.disabled= true;
+                    e.target.nextSibling.disabled=true;
+                    setRemAmt2(moneyTakenByEmp2+remAmt2)
+                    setMoneyTakenByEmp2(0);
+                  }
+                }} />
               <input type="text" onChange={(e)=>{
-                e.target.disabled= true;
-              }} />
+                  if(isNaN(e.target.value)){
+                    alert("Please Enter a Number")
+                    e.target.value="";
+                  }else{
+                    setMoneyTakenByEmp2(Number(e.target.value));
+                  } 
+                }}/>
               </td>
               
-              <td className='amt_att'><input type="checkbox" onChange={(e)=>{
-                if(e.target.checked){
-                  setEmp2(emp2+1);
-                  e.target.disabled= true;
-                }
-              }} />
-              <input type="text" onChange={(e)=>{
-                e.target.disabled= true;
-              }} />
-              </td>
-              
-              <td className='amt_att'><input type="checkbox" onChange={(e)=>{
-                if(e.target.checked){
-                  setEmp3(emp3+1);
-                  e.target.disabled= true;
-                }
-              }} />
-              <input type="text" onChange={(e)=>{
-                e.target.disabled= true;
-              }} />
+              <td className='amt_att'>
+                <input type="checkbox" onChange={(e)=>{
+                  if(e.target.checked){
+                    setEmp3(emp3+1);
+                    e.target.disabled= true;
+                    e.target.nextSibling.disabled=true;
+                    setRemAmt3(moneyTakenByEmp3+remAmt3)
+                    setMoneyTakenByEmp3(0);
+                  }
+                }} />
+                <input type="text" onChange={(e)=>{
+                  if(isNaN(e.target.value)){
+                    alert("Please Enter a Number")
+                    e.target.value="";
+                  } else{
+                    setMoneyTakenByEmp3(Number(e.target.value));
+                  }
+                }}/>
               </td>
             </tr>)
         })}
@@ -70,52 +107,29 @@ function App() {
           <tr>
             <td>Amount</td>
             <td><input type="text" disabled value={rate*emp1}/></td>
+            <td><input type="text" disabled value={rate*emp2}/></td>
             <td><input type="text" disabled value={rate*emp3}/></td>
-            <td><input type="text" disabled value={rate*emp1}/></td>
           </tr>
           <tr>
             <td>MoneyTaken</td>
-            <td><input type="text" disabled/></td>
-            <td><input type="text" disabled/></td>
-            <td><input type="text" disabled/></td>
+            <td>
+              <input type="text" disabled value={remAmt1} />
+            </td>
+            <td>
+              <input type="text" disabled value={remAmt2} />
+            </td>
+            <td>
+              <input type="text" disabled value={remAmt3} />
+            </td>
           </tr>
           <tr>
             <td>RemMoney</td>
-            <td><input type="text" disabled/></td>
-            <td><input type="text" disabled/></td>
-            <td><input type="text" disabled/></td>
+            <td><input type="text" disabled value={(emp1*rate)-remAmt1}/></td>
+            <td><input type="text" disabled value={(emp2*rate)-remAmt2}/></td>
+            <td><input type="text" disabled value={(emp3*rate)-remAmt3}/></td>
           </tr>
         </tfoot>
-        
-
       </table>
-
-
-
-
-
-      {/* <div className='table'>
-        <div className='tr'>
-          <div className='name'>
-            Name
-          </div>
-          {Count().map((e=>{
-            let color = "red";
-            if(e%2===0){
-              color = "blue"
-            }
-            return <div className='td' style={{backgroundColor:color}}>{e}</div>
-          })
-          )}
-          <div className='totalAttendance'>Total Attendance</div>
-          <div className='amt'>Amount</div>
-          <div className='moneyTaken'>Money Taken</div>
-          <div className='RemAmt'>Remaining Amount</div>
-        </div>
-        <EmployeeDetail name="Emp1"/>
-        <EmployeeDetail name="Emp2"/>
-        <EmployeeDetail name="Emp3"/>
-      </div> */}
     </div>
   );
 }
@@ -127,39 +141,4 @@ function Count(){
   }
   return arr;
 }
-
-// function EmployeeDetail(props){
-
-//   return(
-//   <div className='empDetail'>
-//     <div className='name'>
-//       {props.name}
-//     </div>
-//     {/* <Box/> */}
-//     {Count().map(e=> <Box e={e}/>)}
-//     <input type="text" style={{width:"15px", height:"15px"}}/>
-//     <input type="text" style={{width:"15px", height:"15px"}}/>
-//     <input type="text" style={{width:"15px", height:"15px"}}/>
-//     <input type="text" style={{width:"15px", height:"15px"}}/>
-//   </div>)
-// }
-
-// function Box(props){
-//   const amt = useRef();
-
-//   let color = "red"
-//   if(props.e%2===0){
-//     color = "blue"
-//   }
-//   return (<div style={{backgroundColor:color}}>
-//     <input type="checkbox" className='box' onMouseOver={function(e){
-//         amt.current.style.display = "inline";
-//         console.log(e.target)
-//       }}/>
-//     <input type="text" ref={amt} style={{display:"none"}} className='box-text'/>
-//   </div>)
-// }
-
-
-
 export default App;
